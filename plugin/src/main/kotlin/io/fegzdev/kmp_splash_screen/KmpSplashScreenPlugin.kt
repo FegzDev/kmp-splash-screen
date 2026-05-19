0e33c619aec8d05a95482cb251f7e93c19702d78
FegzDev <fegzdev@gmail.com>
2026-05-19 22:15:35 +0100

    chore: setup gradle project with gradle init

---
+++ plugin/src/main/kotlin/io/fegzdev/kmp_splash_screen/KmpSplashScreenPlugin.kt
@@ -0,0 +1,10 @@
+package io.fegzdev.kmp_splash_screen
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class KmpSplashScreenPlugin: Plugin<Project> {
+    override fun apply(project: Project) {
+
+    }
+}
